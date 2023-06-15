2e9a88e890c80c332a2591f19bcb45a40cfa8812
camm2210 <camilodiaza37@gmail.com>
2023-06-15 14:52:33 -0500

    updating controllers

---
+++ src/controllers/paymentsController.js
@@ -0,0 +1,1 @@
+const createOrderController = (req, res) => {};
--- src/routes/payment.js
+++ src/routes/payment.js
@@ -2,8 +2,16 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("pago");
+router.get("/create", (req, res) => {
+  res.send("creating an order");
+});
+
+router.get("/cancel", (req, res) => {
+  res.send("canceling order");
+});
+
+router.get("/capture", (req, res) => {
+  res.send("capturing order");
 });
 
 module.exports = router;
