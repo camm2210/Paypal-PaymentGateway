a9ffd1727908c1ddc1434a08d0da099360269ed8
camm2210 <camilodiaza37@gmail.com>
2023-06-17 19:06:14 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
 const { config } = require("dotenv");
 
 config();
-const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT;
+const PAYPAL_API_CLIENT =
+  "AdiNb6McbAMTW-_Q6gDXi6R-NKPuo6adwdDY7U_a5biMXafZcYdgvf4kC533mTn8or1KeY-rq-YqO2gH";
 
-const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET;
+const PAYPAL_API_SECRET =
+  "EG-Fc_EeXqbYZA8R0FMLxGifVzjLSBaCojFEjUccSd7pqAofsNhKOr2kV4_7tQaEHW84dWm2Qyzau4XE";
 
-const PAYPAL_API = process.env.PAYPAL_API;
+const PAYPAL_API = "https://api-m.sandbox.paypal.com";
 
 module.exports = {
   PAYPAL_API,
